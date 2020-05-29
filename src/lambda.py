647045af24d342d80d7ec073f29ac9e377b9fb8f
grzechu9604 <grzechu96.04@gmail.com>
2020-05-29 12:00:46 +0200

    Lambda code fix

--- src/lambda.py
+++ src/lambda.py
@@ -1,5 +1,11 @@
+import json
 
 def handler(event, context):
+    print('request: {}'.format(json.dumps(event)))
     return {
-        'message': 'Hello World!'
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
     }
