3d636f82bc02f63e67e50bc4f97a97f08bb2aeb4
grzechu9604 <grzechu96.04@gmail.com>
2020-05-30 23:04:46 +0200

    cdk stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import { SoundCarCloudStack } from '../lib/cdk';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new SoundCarCloudStack(app, 'SoundCarCloudStack');
