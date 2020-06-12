49011f0d4bca7526d93aa56a9434b16aad05d020
swolarz <szymon.wol@vp.pl>
2020-06-12 14:41:46 +0200

    Cars elasticsearch schema

--- lambda/src/car-storage/cars_schema.py
+++ lambda/src/car-storage/cars_schema.py
@@ -1,4 +1,49 @@
 schema = {
     'settings': {
+        'number_of_shards': 1,
+        'number_of_replicas': 0
+    },
+    'mappings': {
+        'cars': {
+            'properties': {
+                'ownerId': {
+                    'type': 'keyword'
+                },
+                'engineSoundFile': {
+                    'type': 'keyword'
+                },
+                'carPhotos': {
+                    'properties': {
+                        'main': {
+                            'type': 'keyword'
+                        },
+                        'all': {
+                            'properties': {
+                                'full': {
+                                    'type': 'keyword',
+                                    'thumbnail': 'keyword'
+                                }
+                            }
+                        }
+                    }
+                },
+                'carDescription': {
+                    'type': 'text'
+                },
+                'horsepower': {
+                    'type': 'integer'
+                },
+                'mileage': {
+                    'type': 'integer'
+                },
+                'year': {
+                    'type': 'date',
+                    'format': 'yyyy'
+                },
+                'engine': {
+                    'type': 'text'
+                }
+            }
+        }
     }
 }
