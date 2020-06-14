773c0c0393820aba760dc3da1f8906d90c4dffcf
grzechu9604 <grzechu96.04@gmail.com>
2020-06-15 00:47:15 +0200

    Using ssl

--- lambda/src/car-storage/es_client.py
+++ lambda/src/car-storage/es_client.py
@@ -4,7 +4,7 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 def get_elasticsearch_client(es_endpoint: str) -> Elasticsearch:
     try:
         es = Elasticsearch(
-            hosts=[es_endpoint + ":80"],
+            hosts=[{'host': es_endpoint, 'port': 443, 'use_ssl': True}],
             connection_class=RequestsHttpConnection
         )
 
