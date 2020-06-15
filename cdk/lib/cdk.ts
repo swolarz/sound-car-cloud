e2e43333832e0471e7e8dc2cfd76ac8314688cf5
grzechu9604 <grzechu96.04@gmail.com>
2020-06-15 11:14:19 +0200

    Optimization of timeout

--- cdk/lib/cdk.ts
+++ cdk/lib/cdk.ts
@@ -64,7 +64,7 @@ export class SoundCarCloudStack extends cdk.Stack {
       runtime: lambda.Runtime.PYTHON_3_7,
       code: carStorageCodeAsset,
       handler: 'init_cars_index.handler',
-      timeout: cdk.Duration.minutes(5),
+      timeout: cdk.Duration.minutes(1),
       environment: {
         ELASTICSEARCH_SERVICE_ENDPOINT: elasticsearchDomain.attrDomainEndpoint,
           USER_POOL_ID: userPool.userPoolId,
